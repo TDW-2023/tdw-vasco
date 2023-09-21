f739fd597479ed6060961c1fe4b73f18f606d2f7
Daznar1 <vascopinto.ps@gmail.com>
2023-09-21 10:46:11 +0100

    added helloWorld page

---
+++ app/helloWorld/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const helloWorld = () => {
+  return (
+    <div className='h-screen w-full flex justify-center items-center'>helloWorld</div>
+  )
+}
+
+export default helloWorld
