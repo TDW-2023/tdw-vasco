75387a2d82b37cea7ec8a55c68a6f7d7dc55e098
Vasco Saraiva <vasco.saraiva@ua.pt>
2023-09-28 10:31:05 +0100

    Corrected text

--- app/helloWorld/page.js
+++ app/helloWorld/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const helloWorld = () => {
   return (
-    <div className='h-screen w-full flex justify-center items-center'>helloWorld</div>
+    <div className='h-screen w-full flex justify-center items-center'>Hello World</div>
   )
 }
 
