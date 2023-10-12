28e191808b185b75bcc84bf8c02e8161738fd526
Vasco Saraiva <vasco.saraiva@ua.pt>
2023-10-12 12:43:02 +0100

    Changed a text

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
         />
       </div>
 
-      <Link href={"/helloWorld"}>Clica aqui</Link>
+      <Link href={"/helloWorld"}>Clica aqui 🔥</Link>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
